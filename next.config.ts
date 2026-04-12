1102b82d5f17f772a896a1cca9eb06e96c3ad6e2
udayhasan <udayhasan15@gmail.com>
2026-04-12 23:49:21 +0600

    changes

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: "https://api-staging.madrasadarularqam.org/api/v1/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
